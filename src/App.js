a6da0201384e92b9ea976b311d0f1efebfcad50e
shohan11d <shohan11d@gmail.com>
2024-07-15 04:24:03 +0600

    revamped init

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-import { useState } from "react";
-import { Form } from "./Form";
-import { Logo } from "./Logo";
-import { PackingList } from "./PackingList";
-import { Stats } from "./Stats";
 
 function App() {
-  const [items, setItems] = useState([]);
 
-  function handleAddItems(newItem) {
-    setItems((items) => [...items, newItem]);
-  }
-
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
-
-  function handleClearList(id) {
-    setItems(() => []);
-  }
-  return (
-    <div>
-      <Logo />
-      <Form onAddItems={handleAddItems} />
-      <PackingList
-        items={items}
-        onToggleItem={handleToggleItem}
-        onDeleteItem={handleDeleteItem}
-        onClearList = {handleClearList}
-      />
-      <Stats items={items} />
-    </div>
-  );
+  return <p className="bg-slate-500">Lorem ipsum dolor sit amet.</p>
 }
 
 export default App;
--- src/Logo.js
+++
@@ -1,7 +0,0 @@
-export function Logo() {
-  return (
-    <div className="text-6xl p-10 bg-[#f4a226]  font-bold text-center">
-      <h1>🌲🌴Travel App 💼</h1>
-    </div>
-  );
-}
